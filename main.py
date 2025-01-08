da2a06b701dae11d36ae6df49dcc0472ebe99bc9
Learner Learner <mehemmedmehdi2003@gmail.com>
2025-01-08 23:42:30 +0400

    Training functionalities added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from constants import (
     BATCH_SIZE
     )
 from utils.preprocessing import DataHandler
+from utils.train import Trainer
 from models import Forecaster
 
 loader = DataHandler("../data", seq_length=24, batch_size=BATCH_SIZE)
@@ -10,3 +11,5 @@ for _ in range(10):
     x, y = loader()
     print(x.shape, y.shape)
     break
+# model = Forecaster(input_size)
+# Trainer(model=model, train_loader=loader(), val_loader=loader())
