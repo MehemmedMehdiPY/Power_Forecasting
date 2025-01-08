49aaf4ddc60ea960d64b419d1b5a22df7fcc39df
Learner Learner <mehemmedmehdi2003@gmail.com>
2025-01-08 23:22:55 +0400

    Adding target label

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from constants import (
     BATCH_SIZE
     )
 from utils.preprocessing import DataHandler
-# from models import Forecaster
+from models import Forecaster
 
+loader = DataHandler("../data", seq_length=24, batch_size=BATCH_SIZE)
 
-DataHandler("../data", seq_length=24, batch_size=BATCH_SIZE)
+for _ in range(10):
+    x, y = loader()
+    print(x.shape, y.shape)
+    break
--- utils/preprocessing/data_handler.py
+++ utils/preprocessing/data_handler.py
@@ -14,33 +14,35 @@ class DataHandler(Dataset):
         self.data = df[["Power"]].values
 
         self.column_size = self.data.shape[1]
-        self.row_size = self.data.shape[0] - seq_length
+        self.row_size = self.data.shape[0] - seq_length - 1
 
         self.max_ = torch.tensor(self.data.max(axis=0)).to(torch.float32)
         self.min_ = torch.tensor(self.data.min(axis=0)).to(torch.float32)
 
+    def __call__(self):
+        sample = self.get_sample()
+        sample = torch.tensor(sample).to(torch.float32)
+        sample = (sample - self.min_) / (self.max_ - self.min_)
+        x = sample[:, :self.seq_length]
+        y = sample[:, [self.seq_length]]
+        return x, y
+    
     def get_indexes(self):
         indexes = np.random.choice(self.row_size, self.batch_size)
         return indexes
     
     def get_sample(self):
         indexes = self.get_indexes()
-        sample = np.zeros((self.batch_size, self.seq_length, self.column_size))
+        sample = np.zeros((self.batch_size, self.seq_length + 1, self.column_size))
         for i, idx in enumerate(indexes):
-            sample[i] = self.data[idx: idx + self.seq_length]
+            sample[i] = self.data[idx: idx + self.seq_length + 1]
         return sample
-
-    def __call__(self):
-        x = self.get_sample()
-        x = torch.tensor(x).to(torch.float32)
-        x = (x - self.min_) / (self.max_ - self.min_)
-        return x
 
 
 if __name__ == "__main__":
 
     handler = DataHandler("../../../data/", seq_length=24, batch_size=32)
     print(handler.min_, handler.max_)
-    x = handler()
-    print(x.shape)
+    x, y = handler()
+    print(x.shape, y.shape)
     
